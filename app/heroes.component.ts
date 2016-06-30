1140f7c492212dfac792c81dfaa65cb0c0260222
Jane Ossai <buff_jane@hotmail.com>
2016-06-30 15:13:02 +0100

    added in http stuff, so we fetch the from a server so the full tut is on this branch. This ends this tutorial. putting the app up to http stuff here as its quite complex and irrelevant for now

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -5,12 +5,14 @@ import { HeroService } from './hero.service';
 import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { HeroDetailComponent } from './hero-detail.component';
+
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
-  styleUrls:  ['app/heroes.component.css']
+  styleUrls:  ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
-
 
 
 export class HeroesComponent implements OnInit {
@@ -29,4 +31,27 @@ export class HeroesComponent implements OnInit {
   gotoDetail() {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
+
+  addHero() {
+  this.addingHero = true;
+  this.selectedHero = null;
+}
+
+close(savedHero: Hero) {
+  this.addingHero = false;
+  if (savedHero) { this.getHeroes(); }
+}
+
+
+deleteHero(hero: Hero, event: any) {
+  event.stopPropagation();
+  this.heroService
+      .delete(hero)
+      .then(res => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      })
+      .catch(error => this.error = error);
+}
+
 }
